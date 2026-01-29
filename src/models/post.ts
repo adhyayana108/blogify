5094d67f9722117f3183280e57cbc47331cb87e6
“adhyayana” <adhyayana108@gmail.com>
2026-01-29 23:26:28 +0530

    model

---
+++ src/models/post.ts
@@ -0,0 +1,12 @@
+import {Schema ,model} from 'mongoose';
+
+const postSchema = new Schema({
+    title: {type: String , required: true},
+    content: {type: String , required: true},
+    author: {type: String , required: true},
+    category: {type: String , required: true},
+
+    }, {timestamps: true}
+);
+
+export const Post =  model('Post', postSchema);
